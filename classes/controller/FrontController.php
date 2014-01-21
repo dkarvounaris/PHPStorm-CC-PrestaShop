b47b811140c6796ca7012933e30299127f14f9aa
Dimitrios Karvounaris <d.karvounaris@innovacy.com>
2014-01-21 20:22:18 +0100

    Fixing classes stored in wrong files

--- classes/controller/FrontController.php
+++ classes/controller/FrontController.php
@@ -21,7 +21,7 @@
 *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 */
 
-class AdminController extends AdminControllerCore
+class FrontController extends FrontControllerCore
 {
 
 }
