2d5e2daba32e9da76871707adad715733b91b3fc
Dimitrios Karvounaris <d.karvounaris@innovacy.com>
2014-10-17 05:33:05 +0200

    #Fix

--- classes/ObjectModel.php
+++ classes/ObjectModel.php
@@ -21,7 +21,7 @@
 *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 */
 
-class Object extends ObjectModelCore
+class ObjectModel extends ObjectModelCore
 {
 
 }
